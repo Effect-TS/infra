f4d5855fc3b53e2ca64778144a9575b8463900e2
Michael Arnaldi <ma@matechs.com>
2023-02-15 10:27:32 +0000

    Add vscode server to mike's home

--- users/mikearnaldi/default.nix
+++ users/mikearnaldi/default.nix
@@ -10,6 +10,7 @@
           authorizedKeys = {
             keys = [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEzPT600TtIVU+Ch+sbkB2GuJb+ZScNkOHKhPb6Q8AHv ma@matechs.com"
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFw2nKBMuotlJ8G3CISJXBACkIuiUfDtNGqabs1BMQLG ma@matechs.com"
             ];
           };
         };
--- users/mikearnaldi/home/default.nix
+++ users/mikearnaldi/home/default.nix
@@ -3,6 +3,15 @@
   version,
   ...
 }: {
+  imports = [
+    "${fetchTarball {
+      url = "https://github.com/msteen/nixos-vscode-server/tarball/master";
+      sha256 = "1vgq7141mv67r7xgdpgg54hy41kbhlgp3870kyrh6z5fn4zyb74p";
+    }}/modules/vscode-server/home.nix"
+  ];
+
+  services.vscode-server.enable = true;
+
   home = {
     # Provide home-manager with the information it needs to find the user
     username = "mikearnaldi";
@@ -26,3 +35,4 @@
     };
   };
 }
+
