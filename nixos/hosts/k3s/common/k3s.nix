b48b6f98da537a1f4b7815d5aa10e6fbde4a0cdd
Michael Arnaldi <arnaldimichael@gmail.com>
2023-05-05 23:37:40 +0100

    maybe

--- nixos/hosts/k3s/common/k3s.nix
+++ nixos/hosts/k3s/common/k3s.nix
@@ -67,9 +67,9 @@
         plugins = {
           "io.containerd.grpc.v1.cri" = {
             cni = {
-              bin_dir = "${pkgs.runCommand "cni-bin-dir" {} ''
+              bin_dir = "${"/cni/nix" ''
                 mkdir -p $out
-                ln -sf ${pkgs.cni-plugins}/bin/* ${pkgs.cni-plugin-flannel}/bin/* /cni/custom/* $out
+                ln -sf ${pkgs.cni-plugins}/bin/* ${pkgs.cni-plugin-flannel}/bin/* $out
               ''}";
               conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
             };
