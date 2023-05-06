4df9e5f2f7bf5bfbd8ec4d2fd783b41c3f32c370
Michael Arnaldi <ma@matechs.com>
2023-05-06 20:15:58 +0000

    add lo address

--- nixos/hosts/k3s/common/networking.nix
+++ nixos/hosts/k3s/common/networking.nix
@@ -30,6 +30,16 @@
     };
 
     interfaces = {
+      "lo" = {
+        ipv4 = {
+          addresses = [
+            {
+              address = "0.2.0.1";
+              prefixLength = 32;
+            }
+          ];
+        }
+      };
       "${networkInterface}" = {
         ipv4 = {
           addresses = [
