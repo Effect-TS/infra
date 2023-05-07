01a2d62f108e34885c4c07feaf5bdccb1585c81b
Michael Arnaldi <ma@matechs.com>
2023-05-07 23:19:46 +0000

    fix

--- nixos/hosts/k3s/common/k3s.nix
+++ nixos/hosts/k3s/common/k3s.nix
@@ -65,7 +65,7 @@ in {
         serviceConfig = {
           ExecStartPre = [
             "${pkgs.coreutils}/bin/mkdir -p /var/lib/cni/bin"
-            "${pkgs.coreutils}/bin/ln -sf ${cniBinDir}* /var/lib/cni/bin"
+            "${pkgs.coreutils}/bin/ln -sf ${cniBinDir}/* /var/lib/cni/bin"
           ];
         };
       };
