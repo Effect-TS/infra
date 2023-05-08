19803838f7ea6b99331c84a3b256c33b574d6a50
Michael Arnaldi <ma@matechs.com>
2023-05-08 16:08:36 +0000

    try k8s over ipv6

--- nixos/hosts/k3s/common/k3s.nix
+++ nixos/hosts/k3s/common/k3s.nix
@@ -59,10 +59,10 @@ in {
         "--flannel-backend=none"
         "--disable=coredns"
         "--secrets-encryption"
-        "--node-ip=${networkingConfig.vlanPrivateIPv4}"
-        "--node-external-ip=${networkingConfig.ipv4Address}"
-        "--cluster-cidr=10.4.0.0/16"
-        "--service-cidr=0.3.0.0/16"
+        "--node-ip=${networkingConfig.vlanPrivateIPv6}"
+        "--node-external-ip=${networkingConfig.ipv6Address}"
+        "--cluster-cidr=fdc9:d2b1:7bc2:08e7::/64"
+        "--service-cidr=fdbc:eb0a:5189:84e2::/64"
         "--kube-apiserver-arg 'authorization-mode=Node,RBAC'"
       ];
       tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
