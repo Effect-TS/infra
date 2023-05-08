cccaf5a3f559727b0e1584e2485a4ae1c848ac37
Michael Arnaldi <ma@matechs.com>
2023-05-08 16:20:59 +0000

    restrict range

--- nixos/hosts/k3s/common/k3s.nix
+++ nixos/hosts/k3s/common/k3s.nix
@@ -61,8 +61,8 @@ in {
         "--secrets-encryption"
         "--node-ip=${networkingConfig.vlanPrivateIPv6}"
         "--node-external-ip=${networkingConfig.ipv6Address}"
-        "--cluster-cidr=fdc9:d2b1:7bc2:08e7::/64"
-        "--service-cidr=fdbc:eb0a:5189:84e2::/64"
+        "--cluster-cidr=fdc9:d2b1:7bc2:08e7::/108"
+        "--service-cidr=fdbc:eb0a:5189:84e2::/108"
         "--kube-apiserver-arg 'authorization-mode=Node,RBAC'"
       ];
       tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
