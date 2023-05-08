58d8827315679213d047114cc3a3a029eee64efe
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-05-07 22:57:42 -0400

    auto-configure multus

--- nixos/hosts/k3s/common/k3s.nix
+++ nixos/hosts/k3s/common/k3s.nix
@@ -9,12 +9,33 @@
 }: let
   kubeovn = pkgs.callPackage ./kube-ovn.nix {};
   multuscni = pkgs.callPackage ./multus-cni.nix {};
+  multusConf = lib.generators.toJSON {} {
+    name = "multus-cni-network";
+    type = "multus";
+    kubeconfig = "/etc/rancher/k3s/k3s.yaml";
+    capabilities = {
+      portMappings = true;
+    };
+    delegates = [
+      {
+        type = "kube-ovn";
+        server_socket = "/run/openvswitch/kube-ovn-daemon.sock";
+      }
+      {
+        type = "portmap";
+        capabilities = {
+          portMappings = true;
+        };
+      }
+    ];
+  };
   cniBinDir = pkgs.runCommand "cni-bin-dir" {} ''
     mkdir -p $out
     ln -sf ${pkgs.cni-plugins}/bin/* ${pkgs.cni-plugin-flannel}/bin/* $out
     ln -sf ${kubeovn}/bin/cmd $out/kube-ovn
     ln -sf ${multuscni}/bin/* $out
   '';
+  cniConfDir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
 in {
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "k3s-reset-node" (builtins.readFile ./k3s-reset-node))
@@ -56,6 +77,10 @@ in {
           ExecStartPre = [
             "-${pkgs.zfs}/bin/zfs create -o mountpoint=/var/lib/containerd/io.containerd.snapshotter.v1.zfs zroot/containerd"
           ];
+          ExecStartPost = [
+            "[[ ! -d "${cniConfDir}" ]] && ${pkgs.coreutils}/bin/mkdir -p ${cniConfDir}"
+            "${pkgs.coreutils}/bin/ln -sf ${multusConf} ${cniConfDir}/02-multus.conf"
+          ]
         };
       };
 
@@ -81,7 +106,7 @@ in {
           "io.containerd.grpc.v1.cri" = {
             cni = {
               bin_dir = "${cniBinDir}";
-              conf_dir = "/var/lib/rancher/k3s/agent/etc/cni/net.d/";
+              conf_dir = "${cniConfDir}";
             };
           };
         };
