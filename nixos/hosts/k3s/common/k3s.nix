2877a8c475840a6471e48be7b15b50de40f6ccd0
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-06-04 17:11:39 -0400

    reset k3s

--- nixos/hosts/k3s/common/k3s.nix
+++ nixos/hosts/k3s/common/k3s.nix
@@ -9,37 +9,41 @@
 }: let
   cniBinDir = "/opt/cni/bin";
   cniConfDir = "/etc/cni/net.d";
-  kubeovn = pkgs.callPackage ./kube-ovn-cni.nix {};
-  multusConf = (pkgs.formats.json {}).generate "00-multus.conf" {
-    name = "multus-cni-network";
-    type = "multus";
-    capabilities = {
-      portMappings = true;
-    };
-    delegates = [
-      {
-        name = "kube-ovn";
-        cniVersion = "0.3.1";
-        plugins = [
-          {
-            type = "kube-ovn";
-            server_socket = "/run/openvswitch/kube-ovn-daemon.sock";
-          }
-          {
-            type = "portmap";
-            capabilities = {
-              portMappings = true;
-            };
-          }
-        ];
-      }
-    ];
-    kubeconfig = "/etc/rancher/k3s/k3s.yaml";
-  };
+  # kubeovn = pkgs.callPackage ./kube-ovn-cni.nix {};
+  # multusConf = (pkgs.formats.json {}).generate "00-multus.conf" {
+  #   name = "multus-cni-network";
+  #   type = "multus";
+  #   capabilities = {
+  #     portMappings = true;
+  #   };
+  #   delegates = [
+  #     {
+  #       name = "kube-ovn";
+  #       cniVersion = "0.3.1";
+  #       plugins = [
+  #         {
+  #           type = "kube-ovn";
+  #           server_socket = "/run/openvswitch/kube-ovn-daemon.sock";
+  #         }
+  #         {
+  #           type = "portmap";
+  #           capabilities = {
+  #             portMappings = true;
+  #           };
+  #         }
+  #       ];
+  #     }
+  #   ];
+  #   kubeconfig = "/etc/rancher/k3s/k3s.yaml";
+  # };
+  # cniOriginalBin = pkgs.runCommand "cni-bin-dir" {} ''
+  #   mkdir -p $out
+  #   ln -sf ${pkgs.cni-plugins}/bin/* $out
+  #   ln -sf ${kubeovn}/bin/cmd $out/kube-ovn
+  # '';
   cniOriginalBin = pkgs.runCommand "cni-bin-dir" {} ''
     mkdir -p $out
     ln -sf ${pkgs.cni-plugins}/bin/* $out
-    ln -sf ${kubeovn}/bin/cmd $out/kube-ovn
   '';
 in {
   environment.systemPackages = [
@@ -57,12 +61,13 @@ in {
         "--container-runtime-endpoint=unix:///run/containerd/containerd.sock"
         "--disable=traefik"
         "--disable=coredns"
-        "--flannel-backend=none"
+        # "--flannel-backend=none"
+        "--flannel-backend=wireguard-native"
         "--secrets-encryption"
         "--node-ip=${networkingConfig.vlanPrivateIPv4},${networkingConfig.vlanPrivateIPv6}"
         "--cluster-cidr=10.32.0.0/11,fd01:c26e:7c96:4a4c::/64"
         "--service-cidr=10.64.0.0/12,fdb6:5037:f7b9:190a::/108"
-        "--disable-network-policy"
+        # "--disable-network-policy"
         "--kube-apiserver-arg 'allow-privileged=true'"
         "--kube-apiserver-arg 'authorization-mode=Node,RBAC'"
       ];
@@ -82,6 +87,16 @@ in {
   systemd = {
     services = {
       containerd = {
+        # preStart = ''
+        #   if [[ ! -d "${cniConfDir}" ]]; then
+        #     ${pkgs.coreutils}/bin/mkdir -p "${cniConfDir}"
+        #   fi
+        #   if [[ ! -d "${cniBinDir}" ]]; then
+        #     ${pkgs.coreutils}/bin/mkdir -p /opt/cni/bin
+        #   fi
+        #   ${pkgs.coreutils}/bin/ln -sf ${multusConf} "${cniConfDir}/00-multus.conf"
+        #   ${pkgs.rsync}/bin/rsync -a -L ${cniOriginalBin}/ /opt/cni/bin/
+        # '';
         preStart = ''
           if [[ ! -d "${cniConfDir}" ]]; then
             ${pkgs.coreutils}/bin/mkdir -p "${cniConfDir}"
@@ -89,8 +104,6 @@ in {
           if [[ ! -d "${cniBinDir}" ]]; then
             ${pkgs.coreutils}/bin/mkdir -p /opt/cni/bin
           fi
-          ${pkgs.coreutils}/bin/ln -sf ${multusConf} "${cniConfDir}/00-multus.conf"
-          ${pkgs.rsync}/bin/rsync -a -L ${cniOriginalBin}/ /opt/cni/bin/
         '';
         serviceConfig = {
           ExecStartPre = [
--- nixos/hosts/k3s/host-02/default.nix
+++ nixos/hosts/k3s/host-02/default.nix
@@ -52,7 +52,7 @@ in {
 
     "${modulesPath}/installer/scan/not-detected.nix"
     (import ../common/hardware.nix ({inherit config lib pkgs;} // hardwareConfig))
-    (import ../common/k3s.nix ({inherit config lib pkgs networkingConfig;} // k3sConfig))
+    # (import ../common/k3s.nix ({inherit config lib pkgs networkingConfig;} // k3sConfig))
     (import ../common/networking.nix ({inherit lib pkgs config;} // networkingConfig))
     ../common/nixos.nix
   ];
