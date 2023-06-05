e854070fe50805d46e9ec3906c7203b20058f449
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-06-05 08:36:03 -0400

    remove private ipv4

--- nixos/hosts/k3s/common/k3s.nix
+++ nixos/hosts/k3s/common/k3s.nix
@@ -63,7 +63,6 @@ in {
         # "--disable=coredns"
         # "--flannel-backend=none"
         "--secrets-encryption"
-        "--node-ip=${networkingConfig.vlanPrivateIPv4}"
         # "--node-ip=${networkingConfig.vlanPrivateIPv4},${networkingConfig.vlanPrivateIPv6}"
         # "--cluster-cidr=10.32.0.0/11,fd01:c26e:7c96:4a4c::/64"
         # "--service-cidr=10.64.0.0/12,fdb6:5037:f7b9:190a::/108"
