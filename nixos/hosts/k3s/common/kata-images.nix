d46c50b0299e0594ff60828f37914be0dad5219d
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-06-15 08:45:07 -0400

    add sha

--- nixos/hosts/k3s/common/kata-images.nix
+++ nixos/hosts/k3s/common/kata-images.nix
@@ -8,7 +8,7 @@ in
   fetchzip {
     name = "kata-images-${version}";
     url = "https://github.com/kata-containers/kata-containers/releases/download/${version}/kata-static-${version}-x86_64.tar.xz";
-    sha256 = lib.fakeSha256;
+    sha256 = "sha256-Mqubc1zmj1cJiGe5YTLgSh9XCofDDr7VjQnH0/aQKhc=";
     extraPostFetch = ''
       mv $out/kata/share/kata-containers kata-containers
       rm -r $out
