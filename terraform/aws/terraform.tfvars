83295534f794397de66a5b40f4f3804439bbbccd
Maxwell Brown <maxwellbrown1990@gmail.com>
2023-06-24 10:03:38 -0400

    Add Terraform enforcement (#10)

---
+++ terraform/aws/terraform.tfvars
@@ -0,0 +1,1 @@
+# See https://github.blog/changelog/2022-01-13-github-actions-update-on-oidc-based-deployments-to-aws/
---
+++ terraform/aws/versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
