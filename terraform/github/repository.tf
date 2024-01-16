85099409f213a16bd624cff93f2939b330266fb8
Tim <hello@timsmart.co>
2024-01-16 13:47:18 +1300

    apply allow_rebase_merge

--- terraform/github/repository.tf
+++ terraform/github/repository.tf
@@ -1,13 +1,14 @@
 module "github_repository" {
-  source          = "../modules/github_repository"
-  for_each        = var.repositories
-  name            = each.key
-  description     = each.value.description
-  topics          = each.value.topics
-  homepage_url    = each.value.homepage_url
-  visibility      = each.value.visibility
-  collaborators   = each.value.collaborators
-  pages           = each.value.pages
-  has_discussions = each.value.has_discussions
-  is_archived     = each.value.is_archived
+  source             = "../modules/github_repository"
+  for_each           = var.repositories
+  name               = each.key
+  description        = each.value.description
+  topics             = each.value.topics
+  homepage_url       = each.value.homepage_url
+  visibility         = each.value.visibility
+  collaborators      = each.value.collaborators
+  pages              = each.value.pages
+  has_discussions    = each.value.has_discussions
+  is_archived        = each.value.is_archived
+  allow_rebase_merge = each.value.allow_rebase_merge
 }
