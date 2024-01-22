1a550557b306be071815ee4cd4bb918d5055fa29
Tim <hello@timsmart.co>
2024-01-23 10:13:41 +1300

    add github_branch_protection for next-*

--- terraform/modules/github_repository/branch.tf
+++ terraform/modules/github_repository/branch.tf
@@ -27,3 +27,24 @@ resource "github_branch_protection" "main" {
     required_approving_review_count = 0
   }
 }
+
+
+resource "github_branch_protection" "next-release" {
+  # Branch protection can only be enabled on private repositories if using a
+  # paid GitHub plan
+  count = var.visibility == "public" ? 1 : 0
+
+  repository_id           = github_repository.repository.node_id
+  pattern                 = "next-*"
+  enforce_admins          = true
+  required_linear_history = false
+
+  required_status_checks {
+    strict   = true
+    contexts = null
+  }
+
+  required_pull_request_reviews {
+    required_approving_review_count = 0
+  }
+}
