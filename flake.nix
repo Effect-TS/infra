f0814d1d4a7baf789b800f3183e8cbbb7c753b36
Maxwell Brown <maxwellbrown1990@gmail.com>
2024-10-26 10:16:01 -0400

    update workflows

--- flake.nix
+++ flake.nix
@@ -1,109 +1,68 @@
 {
+  description = "The infrastructure-as-code for Effect";
+
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-23.11";
-    };
-
-    nixpkgs-master = {
-      url = "github:NixOS/nixpkgs/master";
-    };
-
-    nixpkgs-unstable = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
-
-    nixos-hardware = {
-      url = "github:NixOS/nixos-hardware/master";
-    };
-
-    deploy-rs = {
-      url = "github:serokell/deploy-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    home-manager = {
-      url = "github:nix-community/home-manager/release-22.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-colors = {
-      url = "github:misterio77/nix-colors";
-    };
-
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    git-hooks.url = "github:cachix/git-hooks.nix";
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    nixpkgs-master,
-    nixpkgs-unstable,
-    nixos-hardware,
-    deploy-rs,
-    home-manager,
-    nix-colors,
-    sops-nix,
-    ...
-  }: let
-    supportedSystems = [
-      "x86_64-darwin"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "aarch64-linux"
-    ];
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
 
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      imports = [
+        inputs.git-hooks.flakeModule
+      ];
 
-    pkgsFor = system: nixpkgs.legacyPackages.${system};
-    unstablePkgsFor = system: nixpkgs-unstable.legacyPackages.${system};
+      perSystem =
+        {
+          config,
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            inherit (config.pre-commit.devShell) nativeBuildInputs;
 
-    inherit (self) outputs;
-    specialArgs = {inherit inputs outputs;};
-  in {
-    homeManagerModules = import "${self}/nixos/modules/home-manager";
+            buildInputs = with pkgs; [
+              age
+              awscli2
+              findutils
+              opentofu
+              sops
+              ssh-to-age
+              yq-go
+            ];
 
-    formatter = forAllSystems (
-      system: let
-        pkgs = pkgsFor system;
-      in
-        pkgs.alejandra
-    );
+            shellHook = ''
+              ${config.pre-commit.devShell.shellHook}
+            '';
 
-    devShells = forAllSystems (system: let
-      pkgs = unstablePkgsFor system;
-    in {
-      default = pkgs.callPackage "${self}/shell.nix" {inherit pkgs;};
-    });
+            # KUSTOMIZE_PLUGIN_HOME = pkgs.buildEnv {
+            #   name = "kustomize-plugins";
+            #   paths = [
+            #     kustomize-sops
+            #   ];
+            #   postBuild = ''
+            #     mv $out/lib/* $out
+            #     rm -r $out/lib
+            #   '';
+            #   pathsToLink = [ "/lib" ];
+            # };
+          };
 
-    nixosConfigurations = {
-      # On actual machine:
-      #   nixos-rebuild switch --flake .#devbox
-      # On other machine:
-      #   nixos-rebuild --build-host user@host --target-host user@host --use-remote-sudo switch --flake .#devbox
-      # On other machine with dry activation:
-      #   nixos-rebuild --build-host user@host --target-host user@host --use-remote-sudo dry-activate --flake .#devbox
-      devbox = nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = ["${self}/nixos/hosts/devbox"];
-      };
-
-      k3s-host-01 = nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = ["${self}/nixos/hosts/k3s/host-01"];
-      };
-
-      k3s-host-02 = nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = ["${self}/nixos/hosts/k3s/host-02"];
-      };
-
-      k3s-host-03 = nixpkgs.lib.nixosSystem {
-        inherit specialArgs;
-        modules = ["${self}/nixos/hosts/k3s/host-03"];
-      };
+          pre-commit = {
+            settings = {
+              hooks = {
+                # Terraform Hooks
+                terraform-format.enable = true;
+                # Miscellaneous Hooks
+                end-of-file-fixer.enable = true;
+              };
+            };
+          };
+        };
     };
-  };
 }
